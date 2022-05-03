1e8b99a58d821b428cf6f1930bda44d9e8c9ccce
FlorianDelguste <florian.delguste@capgemini.com>
2022-05-03 15:17:58 +0200

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { JsonPipe } from '@angular/common';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
